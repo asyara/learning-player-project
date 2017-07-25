c9c4debdca2982fd13fdb24e7d927104ac6f3bf6
dmitry <asyara@tut.by>
2017-07-25 22:19:43 +0300

    create DTO layer

---
+++ src/main/java/com/yellocode/player/DTO/ArtistDTO.java
@@ -0,0 +1,31 @@
+package com.yellocode.player.DTO;
+
+import com.yellocode.player.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by DmitryL on 23.07.2017.
+ */
+public class ArtistDTO {
+
+    private String groupName;
+
+    private List<User> users;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
