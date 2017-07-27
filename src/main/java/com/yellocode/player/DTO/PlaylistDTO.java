966bdb8f57d8cc1715f388d22366b25acecc13b5
dmitry <asyara@tut.by>
2017-07-27 22:18:23 +0300

    many methods

---
+++ src/main/java/com/yellocode/player/DTO/PlaylistDTO.java
@@ -0,0 +1,7 @@
+package com.yellocode.player.DTO;
+
+/**
+ * Created by DmitryL on 23.07.2017.
+ */
+public class PlaylistDTO {
+}
---
+++ src/main/java/com/yellocode/player/dto/TrackDTO.java
@@ -0,0 +1,7 @@
+package com.yellocode.player.dto;
+
+/**
+ * Created by DmitryL on 18.07.2017.
+ */
+public class TrackDTO {
+}
